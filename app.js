66f3a1b35baaeecc080e08c1c5dc0eecac8fa3d6
Aaron Ealdama <aaronealdama@gmail.com>
2020-03-06 16:30:25 -0600

    fixed pathing issues

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let emptyArr = [];
 // packages
 const fs = require("fs");
 const inquirer = require("inquirer");
+const path = require("path");
 
 // classes
 const Engineer = require("./lib/Engineer");
@@ -24,9 +25,9 @@ const loopNum = () => {
     //   return true;
     // },
     validate: function(num) {
-      if ((/[1-9]/).test(num) === false) {
-        return "Must be a number"
-      } 
+      if (/[1-9]/.test(num) === false) {
+        return "Must be a number";
+      }
       return true;
     }
   });
@@ -40,8 +41,8 @@ const prompt = () => {
       name: "name",
       message: "What is their name?",
       validate: function(input) {
-        if ((/[1-9]/).test(input) === true) {
-          return "Cannot include numbers in name"
+        if (/[0-9]/.test(input) === true) {
+          return "Cannot include numbers in name";
         }
         return true;
       }
@@ -51,8 +52,8 @@ const prompt = () => {
       name: "id",
       message: "What is their id number?",
       validate: function(input) {
-        if ((/[a-zA-z]/).test(input) === true) {
-          return "Cannot include letters in id"
+        if (/[a-zA-z]/.test(input) === true) {
+          return "Cannot include letters in id";
         }
         return true;
       }
@@ -62,8 +63,8 @@ const prompt = () => {
       name: "email",
       message: "What is their email?",
       validate: function(input) {
-        if ((/[@]/).test(input) === false) {
-          return "Must include an email"
+        if (/[@]/.test(input) === false) {
+          return "Must include an email";
         }
         return true;
       }
@@ -79,47 +80,41 @@ const prompt = () => {
 
 const special = obj => {
   if (obj.title === "Manager") {
-    return inquirer
-      .prompt({
-        type: "input",
-        name: "officeNum",
-        message: "What is their office number?",
-        validate: function(input) {
-          if ((/[a-zA-z]/).test(input) === true) {
-            return "Cannot include letters in office number"
-          }
-          return true;
+    return inquirer.prompt({
+      type: "input",
+      name: "officeNum",
+      message: "What is their office number?",
+      validate: function(input) {
+        if (/[a-zA-z]/.test(input) === true) {
+          return "Cannot include letters in office number";
         }
-      })
-      
+        return true;
+      }
+    });
   } else if (obj.title === "Engineer") {
-    return inquirer
-      .prompt({
-        type: "input",
-        name: "github",
-        message: "What is their github username?",
-        validate: function(input) {
-          if ((/[1-9]/).test(input) === true) {
-            return "Cannot include numbers in name"
-          }
-          return true;
+    return inquirer.prompt({
+      type: "input",
+      name: "github",
+      message: "What is their github username?",
+      validate: function(input) {
+        if (/[0-9]/.test(input) === true) {
+          return "Cannot include numbers in name";
         }
-      })
-      
+        return true;
+      }
+    });
   } else if (obj.title === "Intern") {
-    return inquirer
-      .prompt({
-        type: "input",
-        name: "school",
-        message: "What school are they going to?",
-        validate: function(input) {
-          if ((/[1-9]/).test(input) === true) {
-            return "Cannot include numbers in school name"
-          }
-          return true;
+    return inquirer.prompt({
+      type: "input",
+      name: "school",
+      message: "What school are they going to?",
+      validate: function(input) {
+        if (/[0-9]/.test(input) === true) {
+          return "Cannot include numbers in school name";
         }
-      })
-      
+        return true;
+      }
+    });
   } // returns another obj containing the special property
 };
 
@@ -234,7 +229,7 @@ const html = (obj, loop) => {
   </body>
 </html>`;
   return emptyStr; // emptyStr is then returned as a single template literal
-}; 
+};
 
 async function initiation() {
   emptyArr = [];
@@ -242,23 +237,38 @@ async function initiation() {
 
   for (let i = 1; i <= numObj.loop; i++) {
     // for loop loops until numObj.loop value is reached
-    console.log(`This is member ${i}`); 
+    console.log(`This is member ${i}`);
     // alerts user the member number
-    const userObj = await prompt(numObj);
+    const userObj = await prompt();
     // userObj is an obj of user input values
     const specialObj = await special(userObj);
     // specialObj is an obj taking userObj and prompting user a specific question
     // depending on the title
     if (userObj.title === "Manager") {
-      const manager = new Manager(userObj.name, userObj.id, userObj.email, specialObj.officeNum);
+      const manager = new Manager(
+        userObj.name,
+        userObj.id,
+        userObj.email,
+        specialObj.officeNum
+      );
       // new class generated
       emptyArr.push(manager);
       // class object is pushed into an array
     } else if (userObj.title === "Engineer") {
-      const engineer = new Engineer(userObj.name, userObj.id, userObj.email, specialObj.github);
+      const engineer = new Engineer(
+        userObj.name,
+        userObj.id,
+        userObj.email,
+        specialObj.github
+      );
       emptyArr.push(engineer);
     } else if (userObj.title === "Intern") {
-      const intern = new Intern(userObj.name, userObj.id, userObj.email, specialObj.school);
+      const intern = new Intern(
+        userObj.name,
+        userObj.id,
+        userObj.email,
+        specialObj.school
+      );
       emptyArr.push(intern);
     }
   }
@@ -266,11 +276,13 @@ async function initiation() {
   const htmlContent = await html(emptyArr, numObj);
   // returns string with all the html content generated from the content within the emptyArr
 
-  fs.writeFile("team.html", htmlContent, err => {
+  const outputPath = path.resolve(__dirname, "output", "team.html");
+
+  fs.writeFile(outputPath, htmlContent, err => {
     if (err) {
       throw err;
     }
-    console.log("Team HTML is successfully generated!")
+    console.log("Team HTML is successfully generated!");
   }); // team.html is generated with the string from html content
 }
 
